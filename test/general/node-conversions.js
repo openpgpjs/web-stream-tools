2b2df040c551d9169f27079c6f069bab79415e05
Konstantin Goriunov <konstantin.goriunov@headspace.com>
2023-01-05 17:06:46 +0400

    Use this.destroy() to signal error upstream
    As per Node.js documentation https://nodejs.org/api/stream.html#errors-while-reading

--- test/general/node-conversions.js
+++ test/general/node-conversions.js
@@ -19,17 +19,54 @@ async function readStreamToString(readable) {
 }
 
 describe('Node Conversions', function () {
+  let input;
+
+  beforeEach(function () {
+    input = webToNode(nodeToWeb(NodeReadable.from(['a', 'b', 'c'])));
+  });
+
   it('should work', async function () {
-    const input = webToNode(nodeToWeb(NodeReadable.from(['a', 'b', 'c'])));
     const output = await readStreamToString(input);
     expect(output).to.equal('abc');
   });
 
   it('should work within a pipeline', async function () {
-    const input = webToNode(nodeToWeb(NodeReadable.from(['a', 'b', 'c'])));
     const sink = new NodePassThrough();
     await promisify(pipeline)(input, sink);
     const output = await readStreamToString(sink);
     expect(output).to.equal('abc');
+  });
+
+  describe('when underlying readable throws', function () {
+    const mockError = new Error('Oops!');
+
+    beforeEach(function () {
+      function* generator() {
+        yield 'a';
+        yield 'b';
+        throw mockError;
+      }
+      input = webToNode(nodeToWeb(NodeReadable.from(generator())));
+    });
+
+    it('should throw too', async function () {
+      try {
+        await readStreamToString(input);
+        expect.fail();
+      } catch (e) {
+        expect(e).to.equal(mockError);
+      }
+    });
+
+    it('should propagate the error through a pipeline correctly', async function () {
+      try {
+        const sink = new NodePassThrough();
+        await promisify(pipeline)(input, sink);
+        await readStreamToString(sink);
+        expect.fail();
+      } catch (e) {
+        expect(e).to.equal(mockError);
+      }
+    });
   });
 });
