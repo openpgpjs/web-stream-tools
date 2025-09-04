17e4226e1255fb2cc77dcdf2932771df737c7aef
larabr <7375870+larabr@users.noreply.github.com>
2025-09-04 22:01:39 +0200

    TS: add compatibility types for v4.7-v5.4
    And change `test-node` to use mocha with tsx instead of ts-node,
    since the latter has issues with esm module resolutions involving
    typesVersions.
    
    Syntax ref: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html#exports-is-prioritized-over-typesversions

---
+++ lib/types/v4.7-v5.4/index.d.ts
@@ -0,0 +1,29 @@
+/// <reference lib="dom" />
+
+import type { ReadableStream as NodeWebReadableStream } from 'node:stream/web';
+type DomReadableStream<R> = ReadableStream<R>;
+
+type Data = Uint8Array | string;
+
+export type WebStream<T extends Data> = DomReadableStream<T>
+export type NodeWebStream<T extends Data> = NodeWebReadableStream<T>;
+
+type Stream<T extends Data> = WebStream<T> | NodeWebStream<T>;
+type MaybeStream<T extends Data> = T | Stream<T>;
+
+export function readToEnd<T extends Data, JoinFn extends (chunks: T[]) => any = (chunks: T[]) => T>(
+  input: MaybeStream<T>,
+  join?: JoinFn
+): Promise<ReturnType<JoinFn>>;
+
+export function toStream<T extends Data, InputType extends MaybeStream<T>>(
+  input: InputType
+): InputType extends T ? Stream<InputType> : InputType;
+
+export function isStream<T extends Data>(input: MaybeStream<T>): input is Stream<T>;
+
+export function slice<T extends Data, InputType extends MaybeStream<T>>(
+  input: InputType,
+  begin: number,
+  end?: number | typeof Infinity
+): InputType; // same as 'typeof input'
