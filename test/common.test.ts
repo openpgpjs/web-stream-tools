eb256ef910ab92a3743d4f2cc71cfc88c86bf994
Daniel Huigens <d.huigens@protonmail.com>
2025-10-29 17:56:19 +0100

    Fix `pipe`ing a Web Stream to an ArrayStream (#59)

--- test/common.test.ts
+++ test/common.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { toStream, readToEnd, slice } from  '@openpgp/web-stream-tools';
+// @ts-expect-error Missing type definitions
+import { toStream, toArrayStream, readToEnd, slice, pipe, ArrayStream } from '@openpgp/web-stream-tools';
 
 describe('Common integration tests', () => {
   it('toStream/readToEnd', async () => {
@@ -13,5 +14,37 @@ describe('Common integration tests', () => {
     const streamedData = toStream(input);
     const slicedStream = slice(streamedData, 8);
     expect(await readToEnd(slicedStream)).to.equal('chunk');
-  })
-})
+  });
+
+  it('pipe from stream to stream', async () => {
+    const input = 'chunk';
+    const inputStream = toStream(input);
+    const outputStream = new TransformStream();
+    pipe(inputStream, outputStream.writable);
+    expect(await readToEnd(outputStream.readable)).to.equal('chunk');
+  });
+
+  it('pipe from stream to arraystream', async () => {
+    const input = 'chunk';
+    const inputStream = toStream(input);
+    const outputStream = new ArrayStream();
+    pipe(inputStream, outputStream);
+    expect(await readToEnd(outputStream)).to.equal('chunk');
+  });
+
+  it('pipe from arraystream to stream', async () => {
+    const input = 'chunk';
+    const inputStream = toArrayStream(input);
+    const outputStream = new TransformStream();
+    pipe(inputStream, outputStream.writable);
+    expect(await readToEnd(outputStream.readable)).to.equal('chunk');
+  });
+
+  it('pipe from arraystream to arraystream', async () => {
+    const input = 'chunk';
+    const inputStream = toArrayStream(input);
+    const outputStream = new ArrayStream();
+    pipe(inputStream, outputStream);
+    expect(await readToEnd(outputStream)).to.equal('chunk');
+  });
+});
